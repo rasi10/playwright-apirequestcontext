3a726682771ec09c87e9da1499040f98fc6c4915
Rafael Silva <rafael.silva@aimopark.se>
2024-09-23 14:38:04 +0200

    finish demo - v2

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -1,9 +1,64 @@
 import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker';
-import { title } from 'process';
+import { Aircraft, faker } from '@faker-js/faker';
+import { APIHelper } from './apiHelpers';
+import { generateRandomPostPayload } from './testData';
 
 
-test.describe('Test suite backend V1', () => {
+const BASE_URL= 'http://localhost:3000';
+
+test.describe('Test suite backend V2', () => {
+  let apiHelper: APIHelper;
+
+  test.beforeAll(() => {
+    apiHelper = new APIHelper(BASE_URL);
+  })
+
+  test('Test case 01 - Get all posts - v2', async ({ request }) => {
+    const getPosts = await apiHelper.getAllPosts(request);
+    expect(getPosts.ok()).toBeTruthy();
+  });
+
+  test('Test case 02 - create post - v2', async ({ request }) => {
+    const payload = generateRandomPostPayload();
+    const createPostResponse = await apiHelper.createPost(request, payload);
+    expect(createPostResponse.ok()).toBeTruthy();
+
+    // verifying from the POST requestr
+    expect(await createPostResponse.json()).toMatchObject({
+      title: payload.title,
+      views: payload.views
+    })
+
+    // verifying from GET request 
+    const getPosts = await apiHelper.getAllPosts(request);
+    expect(getPosts.ok()).toBeTruthy();
+    expect(await getPosts.json()).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          title: payload.title, 
+          views: payload.views,
+        })
+      ])
+    )
+  });
+
+  test('Test case 03 - Delete Post - v2', async ({ request }) => {
+    const getPosts = await apiHelper.getAllPosts(request);
+    expect(getPosts.ok()).toBeTruthy();
+    const allPosts = await getPosts.json();
+    const lastButOneID = allPosts[allPosts.length - 2].id;
+
+    //Delete request
+    const deleteRequest = await apiHelper.deletePost(request, lastButOneID);
+    expect(deleteRequest.ok()).toBeTruthy();
+
+    // GET by ID and verify status as 404
+    const getPostById = await apiHelper.getByID(request, lastButOneID);
+    expect(getPostById.status()).toBe(404);
+
+
+  });
+  /**
   test('Test case 01 - Get all posts', async ({ request }) => {
     const getPostsResponse = await request.get('http://localhost:3000/posts');
     expect (getPostsResponse.ok()).toBeTruthy();
@@ -58,5 +113,5 @@ test.describe('Test suite backend V1', () => {
     //Verify that the element is gone
     const deletedElementResponse = await request.get(`http://localhost:3000/posts/${lastButOnePostID}`)
     expect (deletedElementResponse.status()).toBe(404);
-  });
+  }); */
 })
